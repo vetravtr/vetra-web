3d0062b62753fa77f1bbadd30816d0c4dcfda7b4
Pedro Fares Ramos <contact@vetravtr.com>
2026-07-06 11:03:53 -0300

    Revert "fix: set turbopack root to dashboard dir"
    This reverts commit c63047bfbebfd48433068428ab2354e88354d635.

--- dashboard/next.config.ts
+++ dashboard/next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  turbopack: {
-    root: process.cwd(),
+  experimental: {
+    optimizePackageImports: ['@walletconnect/ethereum-provider'],
   },
 };
 
